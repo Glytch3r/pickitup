8ecede4f951827c9a8b18037ee0a44a67b85af4b
Glytch3r <glytch3r.arts@gmail.com>
2025-02-05 22:20:51 +0800

    init

---
+++ media/lua/client/PickItUp_Core.lua
@@ -0,0 +1,146 @@
+--[[██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                        				 Custom  PZ  Mod  Developer  for  Hire													  |
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                       	Portfolio:  https://steamcommunity.com/id/glytch3r/myworkshopfiles/							          |
+|                       		                                    														 	  |
+|                       	Discord:    Glytch3r#1337 / glytch3r															      |
+|                       		                                    														 	  |
+|                       	Support:    https://ko-fi.com/glytch3r														    	  |
+|_______________________________________________________________________________________________________________________________-]]
+--[[_____________________________________________________________________________________________________________________________
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████--]]
+
+
+-----------------------            ---------------------------
+PickItUp = PickItUp or {}
+
+function PickItUp.isPlayerOnForageSquare(pl)
+    local px, py = pl:getX(), pl:getY()
+    for _, zone in pairs(forageData) do
+        local bounds = zone.bounds
+        if bounds and px >= bounds.x1 and px <= bounds.x2 and py >= bounds.y1 and py <= bounds.y2 then
+            return true, zone
+        end
+    end
+    return false, nil
+end
+
+function PickItUp.getForageItemAtPlayerSquare(pl)
+    local px, py = pl:getX(), pl:getY()
+    for _, zone in pairs(forageData) do
+        local bounds = zone.bounds
+        if bounds and px >= bounds.x1 and px <= bounds.x2 and py >= bounds.y1 and py <= bounds.y2 then
+            if zone.forageIcons then
+                for _, icon in pairs(zone.forageIcons) do
+                    return icon
+                end
+            end
+        end
+    end
+    return nil
+end
+
+function PickItUp.isNearForageItem(csq)
+    local csqX, csqY = csq:getX(), csq:getY()
+    for _, zone in pairs(forageData) do
+        if zone.forageIcons then
+            for id, icon in pairs(zone.forageIcons) do
+                local dist = math.max(math.abs(csqX - icon.x), math.abs(csqY - icon.y))
+                if dist <= 2 then
+                    return icon, zone, id
+                end
+            end
+        end
+    end
+    return nil
+end
+
+function PickItUp.spawnForageIcon(pl)
+    local px, py = pl:getX(), pl:getY()
+    local icon, zone, id
+    local csq = pl:getCurrentSquare()
+    if PickItUp.isSearching(pl) then
+        if csq then
+            icon, zone, id = PickItUp.isNearForageItem(csq)
+        end
+        if icon and zone and id then
+            local spawn = InventoryItemFactory.CreateItem(icon.itemType)
+            if spawn and csq then
+                local item = csq:AddWorldInventoryItem(spawn, ZombRand(0,2), ZombRand(0,2), 0)
+
+                ISSearchManager:removeItem(icon)
+--[[                 zone.forageIcons[id] = nil
+                zone.itemsLeft = (zone.itemsLeft or 1) - 1
+                ModData.transmit("forageData") ]]
+
+                pl:getXp():AddXP(Perks.PlantScavenging, 1)
+                pl:playSoundLocal("HandShovelHit")
+                if (getCore():getDebug() and isAdmin()) then
+                    pl:addLineChatElement(tostring("forage item spawned: "..tostring(icon.itemType)))
+                end
+            end
+        end
+    end
+end
+
+function PickItUp.isSearching(pl)
+    return ISSearchManager.players[pl] or false
+end
+
+function PickItUp.keypress(key)
+    if key == getCore():getKey("Pick It Up") then
+        local pl = getPlayer()
+        local csq = pl:getCurrentSquare()
+        local canPickForage =  SandboxVars.PickItUp.CanPickUpForageItems or true
+        local canPickNormal =  SandboxVars.PickItUp.CanPickUpWorldInventoryItems or true
+        if canPickForage then
+            PickItUp.spawnForageIcon(pl)
+        end
+        if canPickNormal then
+            if csq then
+                local items = csq:getWorldObjects()
+                for i = 0, items:size() - 1 do
+                    local item = items:get(i):getItem()
+                    if item then
+                        ISTimedActionQueue.add(ISInventoryTransferAction:new(pl, item, item:getContainer(), pl:getInventory()))
+                        break
+                    end
+                end
+            end
+        end
+    end
+    return key
+end
+
+Events.OnKeyPressed.Remove(PickItUp.keypress)
+Events.OnKeyPressed.Add(PickItUp.keypress)
+
+--[[
+function PickItUp.pickUpForageItem(pl, icon, zone, iconId)
+    local item = InventoryItemFactory.CreateItem(icon.itemType)
+    if item then
+        pl:getInventory():AddItem(item)
+        zone.forageIcons[iconId] = nil
+        zone.itemsLeft = (zone.itemsLeft or 1) - 1
+        ModData.transmit("forageData")
+        pl:getXp():AddXP(Perks.PlantScavenging, 1)
+        pl:playSoundLocal("HandShovelHit")
+        return true
+    end
+    return false
+end ]]
