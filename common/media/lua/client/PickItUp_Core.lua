d42fde3d302b47ed04741510748f30ebcab74645
Glytch3r <glytch3r.arts@gmail.com>
2025-02-06 06:28:20 +0800

    up

--- common/media/lua/client/PickItUp_Core.lua
+++ common/media/lua/client/PickItUp_Core.lua
@@ -29,6 +29,55 @@
 -----------------------            ---------------------------
 PickItUp = PickItUp or {}
 
+function PickItUp.isSearching(pl)
+    return ISSearchManager.players[pl] or false
+end
+
+-----------------------            -----------------------------------------------            ---------------------------
+function PickItUp.keypress(key)
+    if key == getCore():getKey("Pick It Up") then
+        local pl = getPlayer()
+        local inv = pl:getInventory()
+        local csq = pl:getCurrentSquare()
+        local canPickForage = SandboxVars.PickItUp.CanPickUpForageItems or true
+        local canPickNormal = SandboxVars.PickItUp.CanPickUpWorldInventoryItems or true
+        if csq then
+            if PickItUp.isSearching(pl) then
+                if canPickForage then
+                    local searchManager = ISSearchManager.getManager(pl)
+                    if searchManager and searchManager.forageIcons then
+                        for _, icon in pairs(searchManager.forageIcons) do
+                            if icon:getIsSeen() and icon:getAlpha() > 0 then
+                                ISTimedActionQueue.add(ISForageAction:new(icon, inv, false))
+                                break
+                            end
+                        end
+                    end
+                end
+            end
+
+            if canPickNormal  then
+                local items = csq:getWorldObjects()
+                for i = 0, items:size() - 1 do
+                    local item = items:get(i):getItem()
+                    if item then
+                        ISTimedActionQueue.add(ISInventoryTransferAction:new(pl, item, item:getContainer(), inv))
+                        break
+                    end
+                end
+            end
+        end
+    end
+    return key
+end
+
+Events.OnKeyPressed.Remove(PickItUp.keypress)
+Events.OnKeyPressed.Add(PickItUp.keypress)
+
+
+
+
+--[[
 function PickItUp.isPlayerOnForageSquare(pl)
     local px, py = pl:getX(), pl:getY()
     for _, zone in pairs(forageData) do
@@ -54,8 +103,9 @@ function PickItUp.getForageItemAtPlayerSquare(pl)
     end
     return nil
 end
-
-function PickItUp.isNearForageItem(csq)
+ ]]
+--[[ function PickItUp.isNearForageItem(csq)
+    csq = csq or getPlayer():getCurrentSquare()
     local csqX, csqY = csq:getX(), csq:getY()
     for _, zone in pairs(forageData) do
         if zone.forageIcons then
@@ -69,39 +119,10 @@ function PickItUp.isNearForageItem(csq)
     end
     return nil
 end
+ ]]
 
-function PickItUp.spawnForageIcon(pl)
-    local px, py = pl:getX(), pl:getY()
-    local icon, zone, id
-    local csq = pl:getCurrentSquare()
-    if PickItUp.isSearching(pl) then
-        if csq then
-            icon, zone, id = PickItUp.isNearForageItem(csq)
-        end
-        if icon and zone and id then
-            local spawn = InventoryItemFactory.CreateItem(icon.itemType)
-            if spawn and csq then
-                local item = csq:AddWorldInventoryItem(spawn, ZombRand(0,2), ZombRand(0,2), 0)
-
-                ISSearchManager:removeItem(icon)
---[[                 zone.forageIcons[id] = nil
-                zone.itemsLeft = (zone.itemsLeft or 1) - 1
-                ModData.transmit("forageData") ]]
-
-                pl:getXp():AddXP(Perks.PlantScavenging, 1)
-                pl:playSoundLocal("HandShovelHit")
-                if (getCore():getDebug() and isAdmin()) then
-                    pl:addLineChatElement(tostring("forage item spawned: "..tostring(icon.itemType)))
-                end
-            end
-        end
-    end
-end
-
-function PickItUp.isSearching(pl)
-    return ISSearchManager.players[pl] or false
-end
-
+-----------------------            -----------------------------------------------
+--[[
 function PickItUp.keypress(key)
     if key == getCore():getKey("Pick It Up") then
         local pl = getPlayer()
@@ -109,7 +130,8 @@ function PickItUp.keypress(key)
         local canPickForage =  SandboxVars.PickItUp.CanPickUpForageItems or true
         local canPickNormal =  SandboxVars.PickItUp.CanPickUpWorldInventoryItems or true
         if canPickForage then
-            PickItUp.spawnForageIcon(pl)
+            --pickup the forage item like how vanilla contextmenu does it but automatic so that it removes the icon data from the table as well as give player the right exp
+            ---PickItUp.spawnForageIcon(pl)
         end
         if canPickNormal then
             if csq then
@@ -128,8 +150,78 @@ function PickItUp.keypress(key)
 end
 
 Events.OnKeyPressed.Remove(PickItUp.keypress)
-Events.OnKeyPressed.Add(PickItUp.keypress)
+Events.OnKeyPressed.Add(PickItUp.keypress) ]]
 
+
+--[[
+function PickItUp.spawnForageIcon(pl)
+    pl = pl or getPlayer()
+    local px, py = pl:getX(), pl:getY()
+    local icon, zone, id
+    local csq = pl:getCurrentSquare()
+    if PickItUp.isSearching(pl) then
+        if csq then
+            icon, zone, id = PickItUp.isNearForageItem(csq)
+        end
+        if icon and zone and id then
+            local spawn = InventoryItemFactory.CreateItem(icon.itemType)
+            if spawn and csq then
+                --local item = csq:AddWorldInventoryItem(spawn, ZombRand(0,2), ZombRand(0,2), 0)
+
+                --ISSearchManager:removeItem(icon)
+                ISSearchManager:doMoveIcon(icon, pl:getX(), pl:getY(), pl:getZ())
+                zone.forageIcons[id] = nil
+                zone.itemsLeft = (zone.itemsLeft or 1) - 1
+                ModData.transmit("forageData")
+
+                pl:getXp():AddXP(Perks.PlantScavenging, 1)
+                pl:playSoundLocal("HandShovelHit")
+                if (getCore():getDebug() and isAdmin()) then
+                    pl:addLineChatElement(tostring("forage item spawned: "..tostring(icon.itemType)))
+                end
+            end
+        end
+    end
+end
+ ]]
+
+
+
+--[[
+-----------------------            ---------------------------
+
+local pl = getPlayer()
+local csq = pl:getCurrentSquare()
+local icon, zone, iconID = PickItUp.isNearForageItem(csq)
+print(icon.zoneData)
+-----------------------            ---------------------------
+
+local pl = getPlayer()
+local csq = pl:getCurrentSquare()
+local manager = ISSearchManager.getManager(pl)
+local isCsqHasIcon =  ISSearchManager:isIconOnSquare(csq, manager.activeIcons)
+if isCsqHasIcon then
+    local icon, zone, iconID = PickItUp.isNearForageItem(csq)
+    local x, y, z = round(csq:getX()),  round(csq:getY()),  csq:getZ()
+
+ ISSearchManager:doMoveIcon(icon, _x, _y, _z)
+   -- ISSearchManager:removeIcon(icon)
+ -- ISTimedActionQueue.add(ISForageAction:new(icon,  getPlayer():getInventory() , false))
+end ]]
+    --[[
+    for iconID, icon in pairs(manager.activeIcons) do
+        if (iconTab[icon.square]) and icon:getIsSeen() then
+            print(icon.zoneData)
+            forageSystem.takeItem(icon.zoneData);
+            forageClient.updateIcon(icon.zoneData, icon.iconID, nil);
+            forageClient.updateZone(icon.zoneData);
+        end;
+    end; ]]
+
+
+
+--ISTimedActionQueue.add(ISForageAction:new(icon, targetContainer, false));
+   --PickItUp.spawnForageIcon(pl)
 --[[
 function PickItUp.pickUpForageItem(pl, icon, zone, iconId)
     local item = InventoryItemFactory.CreateItem(icon.itemType)
